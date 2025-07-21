b0065692f5c045b22078483a496772aa64ae3beb
Seer Pant <spant003@gmail.com>
2025-07-21 19:40:54 +0545

    load pitcher data and handle irrelevant rows/columns

--- ETL/data_clean.py
+++ ETL/data_clean.py
@@ -4,25 +4,26 @@ import pyspark.sql.types as T
 from pyspark.sql.functions import row_number 
 from pyspark.sql.window import Window
 
-
+#function that creates a spark session
 spark = create_spark_session()
 
 #loading batter data along with their hitting statistics
 hitting_df = spark.read.csv("data\\raw\\baseball_hitting.csv", header = True, inferSchema = True)
 
 
-#handling null values and sentinel values
+#handling null values and sentinel values of hitters
 hitting_df = hitting_df.dropna()
-print(f"[INFO] The number of null values is: {hitting_df.filter(F.col("Player name").isNull()).count()}")
+#print(f"[INFO] The number of null values in hitters df is : {hitting_df.filter(F.col("Player name").isNull()).count()}")
 hitting_df = hitting_df.filter( hitting_df["Player name"] != "? Mercado")
 
 
 
 #loading pitchers along with their pitching statistics
 pitching_df = spark.read.csv("data\\raw\\baseball_pitcher.csv", header = True)
+#handling null values of pitchers
+pitching_df = pitching_df.dropna()
+#print(f"[INFO] The number of null values in pitchers df is: {pitching_df.filter(F.col("Player name").isNull()).count()}")
 
-#viewing schemas 
-#hitting_df.printSchema()
 
 #Adding "id" column to hitting_df
 def add_id_column():
@@ -39,8 +40,10 @@ def add_id_column():
 
 hitting_df_final = add_id_column()
 
-#Dropping irrelevant rows 
-hitting_df_final.printSchema()
-hitting_df_final.show() 
+#Confirming validity of data and schemas
+#hitting_df_final.printSchema() 
+#pitching_df.printSchema()
+# hitting_df_final.show() 
+# pitching_df.show()
 
 
